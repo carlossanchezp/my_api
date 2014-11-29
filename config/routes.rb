50349af8daead30823b5fea7c387084fd9aa9c1a
Carlos Sánchez Pérez <csanchez@aspgems.com>
2014-11-29 15:03:05 +0100

    Routes contraints for my api

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   MyApi::Application.routes.draw do
-
+    # Api definition
+    namespace :api, defaults: { format: :json },
+              constraints: { subdomain: 'api' }, path: '/'  do
+      # We are going to list our resources here
+    end
   end
 end
